12ce9e13aecd239819704551caf8c89e500b0d81
muhammhafizh <hafizhsusanto9@gmail.com>
2023-09-06 17:30:27 +0700

    mimikri 0.1

--- app/components/mainHeader.js
+++ app/components/mainHeader.js
@@ -3,7 +3,7 @@ import HeroSection from "/public/Herosection2.svg";
 
 export default function HeaderSection() {
   return (
-    <main className="container mx-auto p-0 md:p-3 md:flex">
+    <main className="container mx-auto p-0 md:p-3 md:flex bg-white">
       <div className="mt-10 md:mt-0">
         <div className="relative -z-30">
           <Image src={HeroSection} width={1550} height={100} />
