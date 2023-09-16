5bef2f0497a1fe08b3fce86b41b37b65668c4272
muhammhafizh <hafizhsusanto9@gmail.com>
2023-09-16 19:44:53 +0700

    mimikri 0.1

--- app/components/partnerSection.js
+++ app/components/partnerSection.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Partner1 from "/public/Partner1.svg";
+import Partner1 from "/public/catalogue jaket dan baju.png";
 import Partner2 from "/public/Partner2.svg";
 import Partner3 from "/public/Partner3.svg";
 import Partner4 from "/public/Maybank.svg";
@@ -25,8 +25,8 @@ export default function PartnerSection() {
   return (
     <div className="w-full container mx-auto">
       <h1 className="text-center font-bold text-3xl">Our Partner</h1>
-      <div className="h-[350px] m-auto overflow-hidden relative w-auto my-7">
-        <ul className="flex w-[calc(250px*14)] animate-scroll pt-5">
+      <div className="h-[250px] m-auto overflow-hidden relative w-auto my-7">
+        <ul className="flex w-[calc(250px*14)] justify-center items-center animate-scroll pt-5">
           {imageData?.map((image, index) => {
             return (
               <li className="w-60" key={index}>
